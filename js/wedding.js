096d575f6ee4693c6623a7b841059afcd4a609b6
fanyy <1178223444@qq.com>
2016-01-11 16:19:23 +0800

    wedding 哈哈

---
+++ js/wedding.js
@@ -0,0 +1,152 @@
+/* wedding
+ */
+
+(function($){
+	$(document).ready(function(){
+	    
+	    // banner
+		//-----------------------------------------------
+		$(".banner-image").backstretch('img/wedding/banner.jpg',{
+			centeredY:false
+		});
+        
+        // flexslider 轮播图
+		//-----------------------------------------------
+		$('.flexslider').flexslider({
+	        animation: "slide",
+	        animationLoop: true,
+	        itemWidth: 414,
+	        itemMargin: 0,
+	        pausePlay: true,
+	        start: function(slider){
+	          //$('body').removeClass('loading');
+	        }
+	      });
+
+		// slider 轮播图
+		//-----------------------------------------------
+		/*var options = { $AutoPlay: true };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);*/
+		
+		// Fixed header
+		//-----------------------------------------------
+		$(window).scroll(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+		$(window).load(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+        //播放器
+        //-----------------------------------------------
+        var ap1 = new APlayer({
+            element: document.getElementById('player1'),
+            narrow: true,
+            autoplay: false,
+            showlrc: false,
+            music: {
+                title: 'Preparation',
+                author: 'Hans Zimmer/Richard Harvey',
+                url: 'img/wedding/Wedding.mp3',
+            }
+        });
+        ap1.init();
+
+
+
+
+
+
+
+
+
+		//Scroll Spy
+		//-----------------------------------------------
+		if($(".scrollspy").length>0) {
+			$("body").addClass("scroll-spy");
+			$('body').scrollspy({ 
+				target: '.scrollspy',
+				offset: 152
+			});
+		}
+
+		//Smooth Scroll
+		//-----------------------------------------------
+		if ($(".smooth-scroll").length>0) {
+			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top-151
+						}, 1000);
+						return false;
+					}
+				}
+			});
+		}
+
+		// Animations
+		//-----------------------------------------------
+		/*if (($("[data-animation-effect]").length>0) && !Modernizr.touch) {
+			$("[data-animation-effect]").each(function() {
+				var $this = $(this),
+				animationEffect = $this.attr("data-animation-effect");
+				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+					$this.appear(function() {
+						setTimeout(function() {
+							$this.addClass('animated object-visible ' + animationEffect);
+						}, 400);
+					}, {accX: 0, accY: -130});
+				} else {
+					$this.addClass('object-visible');
+				}
+			});
+		};*/
+
+		// Isotope filters
+		//-----------------------------------------------
+		if ($('.isotope-container').length>0) {
+			$(window).load(function() {
+				$('.isotope-container').fadeIn();
+				var $container = $('.isotope-container').isotope({
+					itemSelector: '.isotope-item',
+					layoutMode: 'masonry',
+					transitionDuration: '0.6s',
+					filter: "*"
+				});
+				// filter items on button click
+				$('.filters').on( 'click', 'ul.nav li a', function() {
+					var filterValue = $(this).attr('data-filter');
+					$(".filters").find("li.active").removeClass("active");
+					$(this).parent().addClass("active");
+					$container.isotope({ filter: filterValue });
+					return false;
+				});
+			});
+		};
+
+		//Modal
+		//-----------------------------------------------
+		if($(".modal").length>0) {
+			$(".modal").each(function() {
+				$(".modal").prependTo( "body" );
+			});
+		}
+
+	}); // End document ready
+})(this.jQuery);
